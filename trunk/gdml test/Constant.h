3187de4a2082eddef712ac70d71578603b79c878
lee.mephisto@gmail.com <lee.mephisto@gmail.com@c8224bc4-b89a-4393-ea69-a06c113409cd>
2013-10-12 02:06:11 +0000

    gdml test 可以对GDML文件进行可视化处理。

---
+++ trunk/gdml test/Constant.h
@@ -0,0 +1,12 @@
+#pragma once
+//用于常量的定义
+#define DET_THICKNESS 320 //micron
+#define DET_WIDTH 121 
+#define MAX_X 371712//DET_WIDTH*STRIP_NUM
+#define MAX_Y 400000
+#define STRIP_NUM 3072
+#define dEdx 10
+#define PI 3.1415926
+#define NOISE 500
+#define DATA_NUM STRIP_NUM/2
+#define DET_DIS 20000
